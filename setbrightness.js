76b52ade0812ebf7cf7d26e8ea9c3429c89ccf56
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2020-09-03 16:55:12 +0200

    Update setbrightness.js

--- setbrightness.js
+++ setbrightness.js
@@ -8,7 +8,12 @@ device = new HID.HID(4292,33742); // same ID for both boards I got
 // Board has 16 levels, the OS sees 21, so I remap the values and keep the 5 extra at max brightness. 
 // Mappings might be off depending on the driver. Check values on /sys/class/backlight/*/actual_brightness.
 // Was 0-100 by 5 intervals in my case. With the edp kernel patch it was 1-4439 with 222 increments.
-var mapping = [['0', '0'], ['5', '16'],['10', '32'], ['15', '48'], ['20', '64'], ['25', '80'], ['30', '96'], ['35', '112'], ['40', '128'], ['45', '144'], ['50', '160'], ['55', '176'], ['60', '192'], ['65', '208'], ['70', '224'], ['75', '240'], ['80', '240'], ['85', '240'], ['90', '240'], ['95', '240'], ['100', '240']]
+var mapping = [['0', '0'], ['1', '16'],['2', '32'], 
+['3', '48'], ['4', '64'], ['5', '80'], 
+['6', '96'], ['7', '112'], ['8', '128'], 
+['9', '144'], ['10', '160'], ['11', '176'], 
+['12', '192'], ['13', '208'], ['14', '220'], 
+['15', '255']];
 
 var myMap = new Map(mapping);
 
